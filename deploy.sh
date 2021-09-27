d45e0dc44f0eae5655c4bd83140cd8db779f797b
Sean <ussa.say@gmail.com>
2021-09-26 17:59:49 -0700

    Ability to deploy a dist/ folder

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+REMOTE=$(git remote get-url origin)
+# https://stackoverflow.com/a/949391
+LAST_COMMIT=$(git rev-parse HEAD)
+
+# cd to dist/
+mkdir -p dist
+cd dist
+
+# Create a repo with the same remote in dist/ with branch gh-pages
+git init
+git remote add origin $REMOTE
+git fetch origin --depth=1
+# https://stackoverflow.com/a/16595872
+git reset --soft origin/gh-pages
+git checkout -b gh-pages
+
+# Commit everything in dist/ and push
+git add .
+git commit -m "Building for $LAST_COMMIT"
+git push origin gh-pages
+
+# Clean up the repo
+rm -rf .git
