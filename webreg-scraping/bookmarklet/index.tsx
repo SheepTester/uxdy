155f4add7575420f15630eb978db2c948fffb313
Sean <ussa.say@gmail.com>
2022-01-04 00:08:25 -0800

    webreg: Load course data and display while it loads

--- webreg-scraping/bookmarklet/index.tsx
+++ webreg-scraping/bookmarklet/index.tsx
@@ -5,18 +5,36 @@
 /// <reference lib="deno.ns" />
 
 import { render } from 'https://esm.sh/preact@10.6.4'
-import { useState } from 'https://esm.sh/preact@10.6.4/hooks'
-import { AuthorizedGetter } from '../scrape.ts'
+import { useEffect, useState } from 'https://esm.sh/preact@10.6.4/hooks'
+import { AuthorizedGetter, Course } from '../scrape.ts'
+
+const params = new URL(window.location.href).searchParams
+
+// `useEffect` doesn't accept an async function.
+function useAsyncEffect (callback: () => Promise<void>) {
+  useEffect(() => {
+    callback()
+  }, [])
+}
 
 function App () {
-  const [count, setCount] = useState(0)
+  const [courses, setCourses] = useState<Course[]>([])
 
-  return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count => count + 1)}>+1</button>
-    </div>
-  )
+  useAsyncEffect(async () => {
+    const term = params.get('p1')
+    if (!term) {
+      alert("I can't know what quarter you're on based on the URL.")
+      return
+    }
+    const getter = new AuthorizedGetter(term)
+    const courses = []
+    for await (const course of getter.allCourses()) {
+      courses.push(course)
+      setCourses([...courses])
+    }
+  })
+
+  return <ul>{courses.map(course => course.title)}</ul>
 }
 
 const root = document.createElement('div')
