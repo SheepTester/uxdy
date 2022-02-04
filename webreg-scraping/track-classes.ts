847335d29a1c7928af178c7a32fe19d9f3eff866
Sean <ussa.say@gmail.com>
2022-02-04 06:21:53 -0800

    webreg: Generate markdown files for each course It's done! See the results at https://github.com/SheepTester-forks/webreg-data I'll deal with listing joinable courses later

--- webreg-scraping/track-classes.ts
+++ webreg-scraping/track-classes.ts
@@ -3,6 +3,7 @@
 
 import { ensureDir } from 'https://deno.land/std@0.125.0/fs/ensure_dir.ts'
 import { writeAll } from 'https://deno.land/std@0.125.0/streams/conversion.ts'
+import { exams, instructionTypes } from './meeting-types.ts'
 import { AuthorizedGetter } from './scrape.ts'
 
 const QUARTER = 'SP22'
@@ -28,7 +29,7 @@ type HistoryEntry = {
 class CourseHistory {
   entries: HistoryEntry[]
 
-  constructor (entries: HistoryEntry[]) {
+  constructor (entries: HistoryEntry[] = []) {
     this.entries = entries
   }
 
@@ -42,9 +43,11 @@ class CourseHistory {
               const enrollment = sectionEnrollments[section]
               if (enrollment) {
                 const { enrolled, capacity, waitlist } = enrollment
-                return `${enrolled}/${capacity}${
+                return [
+                  enrolled,
+                  capacity === Infinity ? '' : `/${capacity}`,
                   waitlist > 0 ? ` (WL: ${waitlist})` : ''
-                }`
+                ].join('')
               } else {
                 return 'N/A'
               }
@@ -69,13 +72,13 @@ class CourseHistory {
             if (cell === 'N/A') {
               return [sectionCodes[i], null]
             }
-            const match = cell.match(/(\d+)\/(\d+)(?: WL(\d+))?/)
+            const match = cell.match(/(\d+)(?:\/(\d+))?(?: WL(\d+))?/)
             if (!match) {
               throw new SyntaxError(
                 `Couldn't parse enrollment numbers from '${cell}'`
               )
             }
-            const [, enrolled, capacity, waitlist = 0] = match
+            const [, enrolled, capacity = Infinity, waitlist = 0] = match
             return [
               sectionCodes[i],
               { enrolled: +enrolled, capacity: +capacity, waitlist: +waitlist }
@@ -87,20 +90,130 @@ class CourseHistory {
   }
 }
 
-export async function main (sessionIndex: string, uqz: string) {
-  const getter = new AuthorizedGetter(QUARTER, sessionIndex, uqz)
+export async function main (sessionIndex: string, uqz: string, date: string) {
   print(`\r[${' '.repeat(PROGBAR_LENGTH)}]`.padEnd(80, ' '))
+  const getter = new AuthorizedGetter(QUARTER, sessionIndex, uqz)
+  const courseList = await Deno.open('./webreg-data2/courses.md', {
+    write: true,
+    create: true
+  })
+  await courseList.truncate()
+  await writeAll(courseList, encoder.encode(`## Courses (${QUARTER})\n\n`))
   for await (const { course, progress } of getter.allCoursesWithProgress()) {
+    await writeAll(
+      courseList,
+      encoder.encode(
+        `- [**${course.code}**: ${course.title}](./courses/${course.subject}${
+          course.course
+        }.md) (${
+          course.unit.from === course.unit.to
+            ? `${course.unit.to} units`
+            : `${course.unit.from}–${course.unit.to} units, by ${course.unit.step}`
+        })\n`
+      )
+    )
+
+    await Deno.writeTextFile(
+      `./webreg-data2/courses/${course.subject}${course.course}.md`,
+      [
+        `**${course.code}**: ${course.title} (${
+          course.unit.from === course.unit.to
+            ? `${course.unit.to} units`
+            : `${course.unit.from}–${course.unit.to} units, by ${course.unit.step}`
+        })`,
+        '',
+        ...course.groups
+          .sort(
+            // Sort by section code, then non-exams first, then the meeting type
+            (a, b) =>
+              a.code.localeCompare(b.code) ||
+              +a.isExam() - +b.isExam() ||
+              a.type.localeCompare(b.type)
+          )
+          .map(
+            group =>
+              `- **${group.code}** (${
+                group.examType === null
+                  ? instructionTypes[group.instructionType]
+                  : exams[group.examType]
+              }) at ${group.start}–${group.end} on ${group
+                .times()
+                .map(period => period.dayName())
+                .join(', ')} ${
+                group.location
+                  ? `at ${group.location.building} ${group.location.room}`
+                  : '(location TBA)'
+              } by ${
+                group.instructors.length > 0
+                  ? group.instructors.map(
+                      instructor =>
+                        `${instructor.lastFirst} (${instructor.pid})`
+                    )
+                  : 'staff'
+              }`
+          ),
+        '',
+        `[Enrollment numbers over time](./${course.subject}${course.course}.tsv)`
+      ]
+        .map(line => line + '\n')
+        .join('')
+    )
+
+    const filePath = `./webreg-data2/courses/${course.subject}${course.course}.tsv`
+    const history = await Deno.readTextFile(filePath)
+      .then(CourseHistory.parse)
+      .catch(error =>
+        error instanceof Deno.errors.NotFound
+          ? new CourseHistory()
+          : Promise.reject(error)
+      )
+    if (history.entries[history.entries.length - 1]?.date !== date) {
+      history.entries.push({
+        date,
+        sectionEnrollments: Object.fromEntries(
+          course.groups
+            .filter(group => group.plannable)
+            .map(group => [
+              group.code,
+              {
+                enrolled: group.enrolled,
+                capacity: group.capacity,
+                waitlist: group.waitlist
+              }
+            ])
+        )
+      })
+    }
+    await Deno.writeTextFile(
+      filePath,
+      history.toFile(
+        course.groups
+          .filter(group => group.plannable)
+          .sort((a, b) => a.code.localeCompare(b.code))
+          .map(group => group.code)
+      )
+    )
+
     print(
       `\r[${'='.repeat(Math.floor(progress * PROGBAR_LENGTH))}${' '.repeat(
         PROGBAR_LENGTH - Math.floor(progress * PROGBAR_LENGTH)
       )}] ${course.code}`.padEnd(80, ' ')
     )
   }
+  console.log()
 }
 
 if (import.meta.main) {
   // The UqZBpD3n cookie doesn't seem to expire as often, so I put it first
   const [uqz, sessionIndex] = Deno.args
-  await main(sessionIndex, uqz)
+  const today = new Date()
+  await main(
+    sessionIndex,
+    uqz,
+    [
+      today.getFullYear().toString().slice(2),
+      (today.getMonth() + 1).toString().padStart(2, '0'),
+      today.getDate().toString().padStart(2, '0')
+    ].join('')
+  )
 }
