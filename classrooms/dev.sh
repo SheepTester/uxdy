220d51e669777f9cc74694fe7df59c366d481275
Sean <ussa.say@gmail.com>
2023-04-01 17:35:43 -0700

    classrooms: Update map, remove Camp Snoopy

--- classrooms/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
-echo "Serving dist/ at http://localhost:8080/"
-
-http-server ./dist/ -c-1 -s &
-deno bundle --watch ./index.tsx ./dist/index.min.js
