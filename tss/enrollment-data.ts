a241083bb42c42b68897baa7ce1fbc7ebb31217d
sean (costco asus vivobook) <ussa.say@gmail.com>
2026-07-22 23:17:20 -0700

    print enrollment data

---
+++ tss/enrollment-data.ts
@@ -0,0 +1,86 @@
+/**
+ * @file
+ * usage: node tss/enrollment-data.ts
+ */
+
+import { readFile, writeFile } from 'node:fs/promises'
+import type { AllCourses, SectionId } from './index.ts'
+
+const SEPERATOR = '\t'
+
+type ParsedCsv = {
+  header: string[]
+  rows: Record<SectionId, string[]>
+}
+
+function parseCsv (csv: string): ParsedCsv {
+  const [[_, ...header], ...rows] = csv
+    .trim()
+    .split(/\r?\n/)
+    .values()
+    .map(line => line.split(SEPERATOR))
+  return {
+    header,
+    rows: Object.fromEntries(
+      rows.values().map(([sectionId, ...row]) => [sectionId as SectionId, row])
+    )
+  }
+}
+
+function serializeCsv (csv: ParsedCsv): string {
+  let result = `Section ID${SEPERATOR}${csv.header.join(SEPERATOR)}\n`
+  for (const [sectionId, row] of Object.entries(csv.rows).sort((a, b) =>
+    a[0].localeCompare(b[0])
+  )) {
+    result += `${sectionId}${SEPERATOR}${row.join(SEPERATOR)}\n`
+  }
+  return result
+}
+
+function mergeIntoCsv (csv: ParsedCsv, courses: AllCourses): void {
+  const stalenessDates = new Set(
+    courses.values().map(course => course.seat_freshness.label)
+  )
+  if (stalenessDates.size !== 1) {
+    throw new Error(
+      `Multiple staleness dates: ${Array.from(stalenessDates).join(', ')}`
+    )
+  }
+  const [stalenessDate] = stalenessDates
+  if (stalenessDate.includes(SEPERATOR)) {
+    throw new Error('Staleness date contains separator')
+  }
+  let index = csv.header.indexOf(stalenessDate)
+  if (index === -1) {
+    index = csv.header.length
+    csv.header.push(stalenessDate)
+  }
+  for (const row of Object.values(csv.rows)) {
+    while (row.length < csv.header.length) {
+      row.push('')
+    }
+  }
+  for (const course of courses.values()) {
+    for (const section of course.sections) {
+      csv.rows[section.section_id] ??= csv.header.map(() => '')
+      csv.rows[section.section_id][index] = section.seats
+    }
+  }
+}
+
+if (import.meta.main) {
+  const csv = parseCsv(
+    await readFile('tss/enrollment-data.tsv', 'utf-8').catch(error =>
+      Error.isError(error) && 'code' in error && error.code === 'ENOENT'
+        ? ''
+        : Promise.reject(error)
+    )
+  )
+  mergeIntoCsv(
+    csv,
+    new Map(
+      Object.entries(JSON.parse(await readFile('tss/courses.json', 'utf-8')))
+    ) as AllCourses
+  )
+  await writeFile('tss/enrollment-data.tsv', serializeCsv(csv))
+}
